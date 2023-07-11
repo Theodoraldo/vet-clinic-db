0e0b05e85665d7ba203de0ad0c4453ef8c396b9b
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-11 08:09:26 +0000

    Add queries with transaction implementation

--- queries.sql
+++ queries.sql
@@ -13,3 +13,53 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+-- -----------------------------------------------------------------------------------------------------
+
+BEGIN WORK;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK WORK;
+
+SELECT * FROM animals;
+-- -----------------------------------------------------------------------------------------------------
+
+
+BEGIN WORK;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT WORK;
+
+SELECT * FROM animals;
+-- -----------------------------------------------------------------------------------------------------
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+-- -----------------------------------------------------------------------------------------------------
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+
+SAVEPOINT vet_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT vet_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT TRANSACTION;
+
+SELECT * FROM animals;
+-- -----------------------------------------------------------------------------------------------------
